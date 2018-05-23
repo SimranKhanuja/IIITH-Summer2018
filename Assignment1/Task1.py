ab5ec935f3bcf58e338c3f59dce8fb6cecdd4fa5
simi_k <khanuja.simran7@gmail.com>
2018-05-23 23:25:02 +0530

    Update Task1.py

--- Assignment1/Task1.py
+++ Assignment1/Task1.py
@@ -4,8 +4,8 @@ import re
 mydoc = minidom.parse(r'C:\Users\Simran Khanuja\.atom\packages\script\smsCorpus_en_2014.09.06_all.xml')
 toparse = mydoc.getElementsByTagName('text')
 for elem in toparse:
-    x = elem.firstChild.data
-    print(re.findall(r"[A-Z][a-z]+(?=[A-Z])|[\'\w\-\$]+|[\,\.\!\@\#\&\*]+",x))
+    x = elem.firstChild.data.encode('ascii', 'ignore').decode()
+    print(re.findall(r'https?[\.\:\/\w]+|[A-Z][a-z]+(?=[A-Z])|[\'\w\-\$]+|[\,\.\!\@\#\&\*]+',x))
     #[A-Z][a-z]+(?=[A-Z] == AvailableOnly)
     #[\'\w\-\$]+ == any word with - ' and $ preserved
     #[\,\.\!\@\#\&\*]+ == recognizes special characters as tokens as well
