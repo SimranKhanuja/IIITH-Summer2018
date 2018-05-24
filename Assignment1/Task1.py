f17b4cd44eb1ca17dc4c21518da25f6101401363
simi_k <khanuja.simran7@gmail.com>
2018-05-24 16:43:01 +0530

    Update Task1.py

--- Assignment1/Task1.py
+++ Assignment1/Task1.py
@@ -1,11 +1,13 @@
 from xml.dom import minidom
 from nltk import word_tokenize
 import re
-mydoc = minidom.parse(r'C:\Users\Simran Khanuja\.atom\packages\script\smsCorpus_en_2014.09.06_all.xml')
-toparse = mydoc.getElementsByTagName('text')
+mydoc = minidom.parse(r'C:\Users\Simran Khanuja\.atom\packages\script\smsCorpus_en_2014.09.06_all.xml')#importing xml file data
+toparse = mydoc.getElementsByTagName('text')#make objects of "text" tag
 for elem in toparse:
-    x = elem.firstChild.data.encode('ascii', 'ignore').decode()
-    print(re.findall(r'https?[\.\:\/\w]+|[A-Z][a-z]+(?=[A-Z])|[\'\w\-\$]+|[\,\.\!\@\#\&\*]+',x))
-    #[A-Z][a-z]+(?=[A-Z] == AvailableOnly)
-    #[\'\w\-\$]+ == any word with - ' and $ preserved
-    #[\,\.\!\@\#\&\*]+ == recognizes special characters as tokens as well
+    x = elem.firstChild.data.encode('ascii', 'ignore').decode()# get data stored in text tag
+    print(re.findall(r'https?://[\.\w]+|[\w\._%+-]+@[\w\.-]+\.\w+|[A-Z][a-z]+(?=[A-Z])|[\'\w\-\$]+|[\,\.\!\@\#\&\*\/\:]+',x))# tokenization of the data
+    #https?://[\.\w]+ == parsing for urls
+    #[\w\._%+-]+@[\w\.-]+\.\w+ == parsing for email addresses
+    #[\'\w\-\$]+ == any word with special characters preserved
+    #[\,\.\!\@\#\&\*\/\:]+ == recognizes special character as tokens as well
+    
