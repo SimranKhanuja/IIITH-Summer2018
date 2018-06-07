4039eb6ba0bc8d8a0a029b3f7f907b5b1a771ef1
simi_k <khanuja.simran7@gmail.com>
2018-06-07 07:28:04 +0000

    Update EnglishILCI.py

--- Code-Mixed Data/CleaningData/EnglishILCI.py
+++ Code-Mixed Data/CleaningData/EnglishILCI.py
@@ -5,7 +5,7 @@
 
 sentences = []
 sent = ""
-with open("/home/user/Desktop/Code-Mixed Data/Hindi-English-Parallel/ILCI-Corpus/POS-Tagged-English/EnglishICLI.txt", "rt") as in_file: #open file
+with open("/home/user/Desktop/Code-Mixed Data/Hindi-English-Parallel/ILCI-Corpus/POS-Tagged-English/EnglishILCI(Input).txt", "rt") as in_file: #open file
     for line in in_file: # store each line as a string variable
         if line is '\n':
             sentences.append(sent) #storing senetnces
@@ -16,7 +16,7 @@ with open("/home/user/Desktop/Code-Mixed Data/Hindi-English-Parallel/ILCI-Corpus
                 continue
             else:
                 sent = sent + line[0:int(index)] + " " #appending word to make sentence
-outF = open("/home/user/Desktop/Code-Mixed Data/Hindi-English-Parallel/ILCI-Corpus/POS-Tagged-English/EnglishSentences.txt", "w")#exporting output
+outF = open("/home/user/Desktop/Code-Mixed Data/Hindi-English-Parallel/ILCI-Corpus/POS-Tagged-English/EnglishILCI(Output).txt", "w")#exporting output
 for line in sentences:# write line to output file
     outF.write(line)
     outF.write("\n")
