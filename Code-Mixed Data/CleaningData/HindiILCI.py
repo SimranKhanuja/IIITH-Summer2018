c1d6867d2d10e7662f417c3b9f3c1c25542e39d2
simi_k <khanuja.simran7@gmail.com>
2018-06-07 07:43:45 +0000

    Create HindiILCI.py

---
+++ Code-Mixed Data/CleaningData/HindiILCI.py
@@ -0,0 +1,18 @@
+sentences = []
+sent = ""
+with open("/home/user/Desktop/Code-Mixed Data/Hindi-English-Parallel/ILCI-Corpus/POS-Tagged-Hindi/HindiILCI(Input).txt", "rt") as in_file: #open file
+    for line in in_file: # store each line as a string variable
+        if line is '\n':
+            sentences.append(sent) #storing senetnces
+            sent = ""
+        else:
+            index = line.find('\t') #separating word from the pos tag
+            if line[0] in ("|", "?","!") : 
+                continue
+            else:
+                sent = sent + line[0:int(index)] + " " #appending word to make sentence
+outF = open("/home/user/Desktop/Code-Mixed Data/Hindi-English-Parallel/ILCI-Corpus/POS-Tagged-Hindi/HindiILCI(Output).txt", "w")#exporting output
+for line in sentences:# write line to output file
+    outF.write(line)
+    outF.write("\n")
+outF.close()
